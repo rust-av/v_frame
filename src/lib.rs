ada74fee567e5e813be11acb7ddbbea185e20a94
Luca Barbato <lu_zero@gentoo.org>
2020-02-18 10:15:18 +0100

    Split the Frame-related data structures in a separate crate (#2164)
    It will be used by av-metrics and av-scenechange and potentially other separate crates.
    
    Co-authored-by: Vibhoothi <vibhoothiiaanand@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+// Copyright (c) 2020, The rav1e contributors. All rights reserved
+//
+// This source code is subject to the terms of the BSD 2 Clause License and
+// the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+// was not distributed with this source code in the LICENSE file, you can
+// obtain it at www.aomedia.org/license/software. If the Alliance for Open
+// Media Patent License 1.0 was not distributed with this source code in the
+// PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+
+pub mod frame;
+pub mod math;
+pub mod pixel;
+pub mod plane;
+
+mod serialize {
+  cfg_if::cfg_if! {
+     if #[cfg(feature="serialize")] {
+       pub use serde::*;
+      } else {
+        pub use noop_proc_macro::{Deserialize, Serialize};
+     }
+  }
+}
+pub mod prelude {
+  pub use crate::math::*;
+  pub use crate::pixel::*;
+}
